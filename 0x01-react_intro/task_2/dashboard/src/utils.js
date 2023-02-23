b54ba071e33af127050e97522df1bd5a9477c5ef
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-02-23 14:46:54 +0300

    more modifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,20 +1,8 @@
-export function getFullYear() {
-    return new Date().getFullYear();
-};
+export const getFullYear = () => new Date().getFullYear();
 
-export function getFooterCopy(isIndex) {
-    if(isIndex){
-        return 'Holberton School'
-    } else {
-        return 'Holberton School main dashboard'
-    }
-};
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
 
 export const getLatestNotification = () => {
-    return "<strong>Urgent requirement</strong> - complete by EOD";
-  };
-
-// export const getFullYear = () => new Date().getFullYear();
-
-// export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+};
 
