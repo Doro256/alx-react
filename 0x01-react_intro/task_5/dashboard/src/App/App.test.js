e4b3e28796e8502be4dbcddbf78822f17373b89a
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-02-27 15:41:45 +0300

    more modifications

--- 0x01-react_intro/task_5/dashboard/src/App/App.test.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.test.js
@@ -1,30 +1,7 @@
-import React from 'react';
-import App from './App';
-import Enzyme, { shallow } from 'enzyme';
+import App from "./App";
 
-
-describe('App', () => {
-    it('Renders without crashing', () => {
-      const app = shallow(<App />);
-    
-      expect(app).toBeDefined();
-    });
-  });
-
-    it('Renders a div with the class', () => {
-      const app = shallow(<App />);
-    
-      expect(app.find('App-header')).toBeDefined();
-    });
-
-    it('Renders a div with the class', () => {
-      const app = shallow(<App />);
-    
-      expect(app.find('App-body')).toBeDefined();
-    });
- 
-    it('Renders a div with the class', () => {
-      const app = shallow(<App />);
-    
-      expect(app.find('App-footer')).toBeDefined();
-    });
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
