6535ef10666f202915e76d946b2ecb500573238f
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-07 19:28:30 +0300

    modified task_0

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+import "./App.css";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications />
+        <div className="App">
+          <Header />
+          {isLoggedIn ? <CourseList /> : <Login />}
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  } 
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
