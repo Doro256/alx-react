1e0ff899f656289edb7c7f54ce0d333bf8ce69fe
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-14 15:20:51 +0300

    Completed tasks 0 and 1

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,5 @@
+import { fromJS } from 'immutable';
+
+export default getImmutableObject = (object) => {
+    return fromJS(object);
+}
---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,6 @@
+import { Map } from 'immutable';
+
+
+export default getImmutableObject = (object) => {
+    return Map(object);
+}
