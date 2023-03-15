0c3d28968ee835c2063f17e96d415519d6f7777a
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-15 15:33:01 +0300

    modified 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    const objval = fromJS(object);
+    const objVal = fromJS(object);
 
     return objVal.getIn(array,undefined);
 }
