d6b0ca401f466539b343918b3ca7b3469a50afe3
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-17 18:28:09 +0300

    Fixed lint errors

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,5 @@
 import { fromJS } from 'immutable';
 
-export default getImmutableObject = (object) => {
-    return fromJS(object);
+export default function getImmutableObject(object) {
+  fromJS(object);
 }
