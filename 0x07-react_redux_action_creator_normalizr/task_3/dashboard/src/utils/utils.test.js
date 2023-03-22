20ff720ea1c6b84b8fd9715be105bbeb6eb14173
Dorothy Nantumbwe <88768289+Doro256@users.noreply.github.com>
2023-03-22 18:57:46 +0300

    task_3 done

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/utils/utils.test.js
@@ -0,0 +1,15 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
+
+test("returns the current year", () => {
+    expect(getFullYear()).toBe(2023);
+});
+
+test("returns the correct footer", () => 
+{
+    expect(getFooterCopy(true)).toBe("Holberton School");
+    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+});
+
+test("returns right notification", () => {
+    expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+});
